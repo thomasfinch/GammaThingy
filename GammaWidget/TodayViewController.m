4c504aff454b519a897520f9858b1ea350c01c07
Arthur Hammer <arthur@ahammer.me>
2015-10-27 09:40:36 +0100

    Add Widget Target

---
+++ GammaWidget/TodayViewController.m
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.m
+//  GammaWidget
+//
+//  Created by Arthur Hammer on 27.10.15.
+//  Copyright © 2015 Thomas Finch. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
