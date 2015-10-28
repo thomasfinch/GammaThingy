ba4be2810f288975c4f6c8a45ce5bda2ae17f3d1
Arthur Hammer <arthur@ahammer.me>
2015-10-28 11:17:54 +0100

    Add widget controller logic

--- GammaWidget/TodayViewController.m
+++ GammaWidget/TodayViewController.m
@@ -8,31 +8,90 @@
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
+#import "GammaController.h"
+#import "NSUserDefaults+Group.h"
 
 @interface TodayViewController () <NCWidgetProviding>
-
+@property IBOutlet UIButton *toggleButton;
+@property IBOutlet UIButton *increaseButton;
+@property IBOutlet UIButton *decreaseButton;
 @end
 
 @implementation TodayViewController
 
+float increaseOrangenessBy = 0.1f;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [GammaController autoChangeOrangenessIfNeeded];
+    [self updateUI];
+}
+
+- (void)updateUI {
+    BOOL enabled = [[NSUserDefaults groupDefaults] boolForKey:@"enabled"];
+    self.toggleButton.selected = enabled;
+    self.increaseButton.enabled = enabled;
+    self.decreaseButton.enabled = enabled;
+}
+
+- (IBAction)toggle:(id)sender {
+    if ([[NSUserDefaults groupDefaults] boolForKey:@"enabled"]) {
+        [GammaController disableOrangeness];
+    } else {
+        [GammaController enableOrangeness];
+    }
+    [self updateUI];
+}
+
+- (IBAction)increase:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults groupDefaults];
+    
+    if ([defaults boolForKey:@"enabled"]) {
+        float intensity = [defaults floatForKey:@"maxOrange"];
+        float newIntensity = intensity + increaseOrangenessBy;
+        newIntensity = newIntensity >= 1 ? 1 : newIntensity;
+        [GammaController setGammaWithTransitionFrom:intensity to:newIntensity];
+        // This should be somewhere else, no?
+        [defaults setFloat:newIntensity forKey:@"maxOrange"];
+        [defaults synchronize];
+        [self updateUI];
+    }
+}
+
+- (IBAction)decrease:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults groupDefaults];
+    
+    if ([defaults boolForKey:@"enabled"]) {
+        float intensity = [defaults floatForKey:@"maxOrange"];
+        float newIntensity = intensity - increaseOrangenessBy;
+        newIntensity = newIntensity <= 0 ? 0 : newIntensity;
+        [GammaController setGammaWithTransitionFrom:intensity to:newIntensity];
+        // This should be somewhere else, no?
+        [defaults setFloat:newIntensity forKey:@"maxOrange"];
+        [defaults synchronize];
+        [self updateUI];
+    }
+}
+
+- (IBAction)openApp:(id)sender {
+    [[self extensionContext] openURL:[NSURL URLWithString:@"gammathingy://"] completionHandler:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    // Perform any setup necessary in order to update the view.
+    NSUserDefaults *defaults = [NSUserDefaults groupDefaults];
+    BOOL enabledOnLastCheck = [defaults boolForKey:@"widgetLastCheckEnabled"];
+    BOOL enabled = [defaults boolForKey:@"enabled"];
+    [defaults setBool:enabled forKey:@"widgetLastCheckEnabled"];
+    [defaults synchronize];
     
-    // If an error is encountered, use NCUpdateResultFailed
-    // If there's no update required, use NCUpdateResultNoData
-    // If there's an update, use NCUpdateResultNewData
-
-    completionHandler(NCUpdateResultNewData);
+    completionHandler(enabledOnLastCheck != enabled ? NCUpdateResultNewData : NCUpdateResultNoData);
 }
 
 @end
